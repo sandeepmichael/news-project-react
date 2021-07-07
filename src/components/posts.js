899edbdedf54cff17f5495d8cfef9a7732e251c3
unknown <mahadarisandeep@gmail.com>
2021-07-07 18:40:14 +0530

    second commit

--- src/components/posts.js
+++ src/components/posts.js
@@ -11,8 +11,8 @@ const Posts = ({posts, loading}) => {
     return (
     <div className='App'>
         <ul className='List-group mb-4'>
-        {posts.map(post => (
-          <li key={post.id} className='List-group-item' style={{border:'1px solid black'}}>
+        {posts.map((post, i) => (
+          <li key={i} className='List-group-item' style={{border:'1px solid black'}}>
               <a href={post.url}><h1>{post.title}</h1></a>
               <p>{post.description}</p>
               <img className='news-img' style={{padding:'1%'}} width={400} src={post.urlToImage} alt='NewImage' />
